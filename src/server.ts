50776134899f5020a5feb73f23c572e69e6cb366
João Victor <dev.joaovictor@hotmail.com>
2023-11-10 14:23:00 -0300

    feat: add dotenv config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
 
 import { Router, Request, Response } from 'express';
 
+dotenv.config();
 const app = express();
 
 const route = Router();
@@ -13,4 +15,4 @@ route.get('/', (req: Request, res: Response) => {
 });
 
 app.use(route);
-app.listen(3333, () => 'server running on port 3333');
+app.listen(process.env.PORT, () => 'server running on port 3333');
