735e0f329ed556217fd7013a1c667b947a107d71
João Victor <dev.joaovictor@hotmail.com>
2023-11-22 10:53:18 -0300

    feat: add client repository

---
+++ src/repositories/client-repository.ts
@@ -0,0 +1,52 @@
+import { CreateClientRequest } from '../entities';
+import { prismaClient } from '../lib/prisma';
+
+export class ClientRepository {
+  async findById(id: string) {
+    return await prismaClient.client.findUnique({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async findByName(name: string) {
+    return await prismaClient.client.findFirst({
+      where: {
+        name: name,
+      },
+    });
+  }
+
+  async findAll() {
+    return await prismaClient.client.findMany();
+  }
+
+  async create(client: CreateClientRequest) {
+    return await prismaClient.client.create({
+      data: {
+        name: client.name,
+        id: client.id,
+      },
+    });
+  }
+
+  async delete(id: string) {
+    return await prismaClient.client.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async update(id: string, client: CreateClientRequest) {
+    return await prismaClient.client.update({
+      where: {
+        id: id,
+      },
+      data: {
+        name: client.name,
+      },
+    });
+  }
+}
