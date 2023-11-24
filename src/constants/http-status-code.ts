1a7c22bedf902dbb6b8854848b140e4f47c56565
João Victor <dev.joaovictor@hotmail.com>
2023-11-24 18:11:41 -0300

    feat: add HttpStatusCode enum

---
+++ src/constants/http-status-code.ts
@@ -0,0 +1,8 @@
+export class HttpStatusCode {
+  static readonly OK = 200;
+  static readonly CREATED = 201;
+  static readonly BAD_REQUEST = 400;
+  static readonly NOT_FOUND = 404;
+  static readonly CONFLICT = 409;
+  static readonly INTERNAL_SERVER_ERROR = 500;
+}
