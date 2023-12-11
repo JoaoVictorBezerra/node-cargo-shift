cc6021a97b88be448880353065cb4dc29ba10949
João Victor <dev.joaovictor@hotmail.com>
2023-12-11 15:02:14 -0300

    chore: changed code style from http status code

--- src/constants/http-status-code.ts
+++ src/constants/http-status-code.ts
@@ -1,8 +1,8 @@
-export class HttpStatusCode {
-  static readonly OK = 200;
-  static readonly CREATED = 201;
-  static readonly BAD_REQUEST = 400;
-  static readonly NOT_FOUND = 404;
-  static readonly CONFLICT = 409;
-  static readonly INTERNAL_SERVER_ERROR = 500;
+export enum HttpStatusCode {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  CONFLICT = 409,
+  INTERNAL_SERVER_ERROR = 500,
 }
