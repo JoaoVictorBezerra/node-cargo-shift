48671168263da93df7959f7856bd67be22db02ae
João Victor <dev.joaovictor@hotmail.com>
2023-12-13 12:21:32 -0300

    chore: added login validations

--- src/helpers/validations.ts
+++ src/helpers/validations.ts
@@ -8,7 +8,21 @@ function clientParams() {
 
 function uuidParams() {
   return z.object({
-    id: z.string().uuid(),
+    id: z
+      .string({ required_error: 'ID is required' })
+      .uuid({ message: 'Please, enter a valid UUID' }),
+  });
+}
+
+function loginParams() {
+  return z.object({
+    name: z.string({ required_error: 'Name is required' }),
+    // email: z.string().email(),
+    password: z
+      .string({ required_error: 'Password is required' })
+      .trim()
+      .min(6, { message: 'Password must be at least 6 characters' })
+      .max(50, { message: 'Password must be at most 50 characters' }),
   });
 }
 
@@ -16,4 +30,5 @@ export {
   clientParams as CreateClientSchema,
   clientParams as UpdateClientSchema,
   uuidParams as uuidSchema,
+  loginParams as LoginSchema,
 };
